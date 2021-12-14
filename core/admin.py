fa90bb79572e16ee053ef9118cf7dc517861a9ed
Matheus <matheus.mignon@gmail.com>
2021-12-14 17:50:25 -0300

    Create Admin models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Employee, Service, Position
 
-# Register your models here.
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('position', 'active', 'modified')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'icon', 'active', 'modified')
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'active', 'modified')
